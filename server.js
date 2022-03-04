f271aaa8a3c65aaefa0d0efca70ed0cf484f7364
akayode <akayode@live.unc.edu>
2022-03-04 02:24:44 -0500

    Working endpoints

--- server.js
+++ server.js
@@ -17,6 +17,69 @@ app.get('/app', (req, res) => {
     res.type('text/plain')
 });
 
+function coinFlip() {
+    let flip = Math.random();
+    if (flip < 0.5) {
+      var result = "heads";
+    } else {
+      var result = "tails";
+    }
+    return result;
+}
+
+app.get('/app/flip', (req, res) => {
+    var flip = coinFlip()
+    res.status(200).json({ 'flip' : flip })
+});
+
+function coinFlips(flips) {
+    var flip_array = [];
+    for (var i = 0; i < flips; i ++) {
+      flip_array.push(coinFlip());
+    }
+    return flip_array
+}
+
+function countFlips(array) {
+    let heads = 0;
+    let tails = 0;
+    for (const outcome of array) {
+      if (outcome == "heads") {
+        heads += 1;
+      } else {
+        tails += 1;
+      }
+    }
+    if (heads > 0 && tails >0) {
+      var output = `{ heads: ${heads}, tails: ${tails} }`;
+    } else if (heads > 0 && tails == 0) {
+      var output = `{ heads: ${heads} }`;
+    } else {
+      var output = `{ tails: ${tails} }`;
+    }
+    return output;
+}
+
+app.get('/app/flips/:number', (req, res) => {
+    const flips = coinFlips(req.params.number)
+    const summary = countFlips(flips)
+    res.status(200).json({"raw": flips, "summary": summary})
+});
+
+function flipACoin(call) {
+    var hidden_filp = coinFlip();
+    if (call == hidden_filp) {
+        return { call: call, flip: hidden_filp, result: 'win' };
+    } else {
+        return { call: call, flip: hidden_filp, result: 'lose' };
+    } 
+}
+
+app.get('/app/flip/call/:guess', (req, res) => {
+    const result = flipACoin(req.params.guess)
+    res.status(200).json(result)
+});
+
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
     res.type("text/plain")
